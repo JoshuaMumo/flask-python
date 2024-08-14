76eeec8c8860593f0bddb3269be2f84ce1515e4d
jmumo <jmumo6672@gmail.com>
2024-08-14 16:55:06 +0300

    flask python

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/index')
+def index():
+    return 'Index Page'
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
